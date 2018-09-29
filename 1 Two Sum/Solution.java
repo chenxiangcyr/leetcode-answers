36ecdfd32eb7125bd1cc89541b54ac08d001710f
Xiang Chen <xianch@amazon.com>
2018-09-29 11:15:00 +0800

    1

---
+++ 1 Two Sum/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        // key: integer, value: the idx for the integer
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        
+        for(int i = 0; i < nums.length; i++) {
+            int rest = target - nums[i];
+            
+            if(map.containsKey(rest)) {
+                return new int[] {i, map.get(rest)};
+            }
+            
+            map.put(nums[i], i);
+        }
+        
+        throw new IllegalArgumentException("no result for the input");
+    }
+}
